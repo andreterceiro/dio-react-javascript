c45960b4d2c29cece8b244418cac54eacc84c132
André de Paula Terceiro <andre@terceiro.com.br>
2024-11-27 09:54:33 -0300

    feat: organizing some-tests.js

--- some-tests.js
+++ some-tests.js
@@ -1,24 +1,35 @@
-/*
-var a = 10;
-let b = 5;
-function teste() {
+function test1() {
+    var a = 10;
+    let b = 5;
+    function teste() {
+        console.log(a);
+        a = 11;
+        console.log(b);
+        b = 12;
+    }
+    teste();
     console.log(a);
-    a = 11;
     console.log(b);
-    b = 12;
 }
-teste();
-console.log(a);
-console.log(b);
-*/
 
-values = [1, 2, 3];
+function test2() {
+    values = [1, 2, 3];
 
-values.forEach(value => {
-    console.log(value);
-})
+    values.forEach(value => {
+        console.log(value);
+    })
 
-doubles = values.map((value) => {
-    return value * 2;
-});
-console.log(doubles);
+    doubles = values.map((value) => {
+        return value * 2;
+    });
+    console.log(doubles);
+}
+
+function test3() {
+    values = [1,2,3,4];
+    console.log(values.filter((value) => value > 2));
+}
+
+// test1();
+// test2();
+test3();
