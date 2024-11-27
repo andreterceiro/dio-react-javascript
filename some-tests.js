feb6c89800e02c5c093d3f8ce2a3e112ae31d79b
André de Paula Terceiro <andre@terceiro.com.br>
2024-11-27 09:33:25 -0300

    feat: a test related to foreach and another test related to map

--- some-tests.js
+++ some-tests.js
@@ -1,3 +1,4 @@
+/*
 var a = 10;
 let b = 5;
 function teste() {
@@ -9,3 +10,15 @@ function teste() {
 teste();
 console.log(a);
 console.log(b);
+*/
+
+values = [1, 2, 3];
+
+values.forEach(value => {
+    console.log(value);
+})
+
+doubles = values.map((value) => {
+    return value * 2;
+});
+console.log(doubles);
